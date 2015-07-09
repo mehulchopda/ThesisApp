71939dc9006d8dd3b4684584195454ff32c10632
mehulchopda000 <mehul.chopda@yahoo.com>
2015-07-09 14:42:23 +0200

    Initial Commit

---
+++ ThesisApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThesisApp
+//
+//  Created by Mehul Chopda on 09/07/15.
+//  Copyright (c) 2015 Mehul Chopda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
